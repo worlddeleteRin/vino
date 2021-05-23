0d4c35b9b72b1a01a6f5fd47e15ac02db9b0d0e4
Rin NoName <noname@MacBook-Pro-Rin.lan>
2021-05-23 15:32:13 +0300

    new commit

--- main/views.py
+++ main/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 # Create your views here.
 
 def index (request):
     template = 'main/base.html'
-    context = {}
+    context = {
+	}
     return render(request, template, context)
 
 def app_client (request):
     template = 'main/base.html'
     context = {}
     return render(request, template, context)
+
